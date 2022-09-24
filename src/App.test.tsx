c8b51893dd579a31fadd95b7dbc158113e314aa8
Robles Lautaro <lautaro0121@gmail.com>
2022-09-24 19:39:25 -0300

    asd

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,6 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-<<<<<<< Updated upstream
-import { store } from '../src/redux/store';
-=======
 import { store } from './redux/store';
->>>>>>> Stashed changes
 import App from './App';
 
 
@@ -13,13 +9,9 @@ import App from './App';
       <App />
     </Provider>
   );
-<<<<<<< Updated upstream
-});
-=======
 
   describe('App', () => {
     it('should render', () => { 
         expect(view).toBeTruthy()
     })
   })
->>>>>>> Stashed changes
