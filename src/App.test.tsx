585594f3aa41c417b058c178c502ff5cd0947d4f
Robles Lautaro <lautaro0121@gmail.com>
2022-09-24 16:24:31 -0300

    ads

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from '../src/redux/store';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -10,6 +10,4 @@ test('renders learn react link', () => {
       <App />
     </Provider>
   );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
 });
