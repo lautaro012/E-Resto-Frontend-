3234c0c6916e9f1743a72203cc0996c6152dfeb2
Sergio Romero <phyrofyre@gmail.com>
2022-10-02 17:18:37 -0300

    modify card,app

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,62 +1,85 @@
 import { Link } from "react-router-dom";
 import { useAppDispatch } from "../../config";
 import { deleteProduct } from "../../redux/actions";
-import '../Card/Card.css'
+import { Card } from "flowbite-react/lib/esm/components";
+import PrettyRating from "pretty-rating-react";
+import {
+    faStar,
+    faStarHalfAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+    faStar as farStar,
+} from "@fortawesome/free-regular-svg-icons";
 
-export default function Card({formCard=false, comidaProps, onProducEdit}: any) {
-    let { _id, price, off, description, name, img } = comidaProps
+export default function CardProduct({ formCard = false, comidaProps, onProducEdit }: any) {
+    let { _id, price, off, description, name, img, rating } = comidaProps
     let dispatch = useAppDispatch()
     let currentprice = price - (price * off / 100)
-    
+
     const handleEdit = () => {
-        console.log(comidaProps, 'aca')
         onProducEdit(comidaProps)
     }
 
     const handleDelete = () => {
         dispatch(deleteProduct(_id))
     }
+
+    const icons = {
+        star: {
+            complete: faStar,
+            half: faStarHalfAlt,
+            empty: farStar,
+        }
+    }
+    const colors = {
+        star: ['#d9ad26', '#d9ad26', '#434b4d'],
+    }
+
+    console.log("COMIDAS PROPS", comidaProps)
+
     return (
-        <section className="section">
-            <div className="container">
-                <div className="grid">
-                    <article className="card">
-                        <Link to={`/product/${_id}`}>
-                            <img className="card__image" src={img} alt={name} width="200" height="200" />
-                        </Link>
-                        <div className="card__data">
-                            <div className="card__info">
-                                <Link to={`/product/${_id}`}>
-                                    <h2>{name}</h2>
-                                </Link>
-                                <p>{description}</p>
+        <div className="max-w-sm">
+            <Card
+                imgAlt={name}
+                imgSrc={img}
+            >
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                    {description}
+                </h5>
+                <div className="mt-2.5 mb-5 flex items-center">
+                    <PrettyRating value={rating} colors={colors.star} />
+                    <span className="mr-2 ml-3 rounded bg-blue-100 px-2.5 py-0.5 text-xs font-semibold text-blue-800 dark:bg-blue-200 dark:text-blue-800">
+                        {rating}
+                    </span>
+                </div>
+                <div className="flex items-center justify-between">
+
+                    {
+                        !off ?
+                            <span className="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+                            :
+                            <div className="off_price">
+                                <span className="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+                                <div className="tag_off">
+                                    <span className="text-3xl font-bold text-gray-900 dark:text-white">${currentprice}</span>
+                                </div>
                             </div>
-                            {
-                                !off ?
-                                    <h3 className="card__price">$ {price}</h3>
-                                    :
-                                    <div className="off_price">
-                                        <del><h3 className="card__price">$ {price}</h3></del>
-                                        <div className="tag_off">
-                                            <h3 className="card_price">{off}%</h3>
-                                        </div>
-                                        <h3 className="card_price"> $ {currentprice}</h3>
-                                    </div>
-                            }
-                        </div>
-                    </article>
+                    }
+
+                    {
+                        formCard ?
+                            null
+                            :
+                            <div>
+                                <Link className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" to={`/product/${_id}`}>
+                                    Agregar al carrito
+                                </Link>
+                                <button className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleEdit}>edit</button>
+                                <button className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleDelete}>delete</button>
+                            </div>
+                    }
                 </div>
-            </div>
-            {
-                formCard ?
-                null
-                :
-                <div className="card-buttons">
-                <button onClick={handleEdit}>edit</button>
-                <button onClick={handleDelete}>delete</button>
-                </div>
-            }
-            
-        </section>
+            </Card>
+        </div>
     )
 }
