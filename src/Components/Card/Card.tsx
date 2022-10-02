f9f454f6d1dd3c01012701fb14a93fcc9c6eedb9
Sergio Romero <phyrofyre@gmail.com>
2022-10-02 17:53:25 -0300

    card

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useAppDispatch } from "../../config";
 import { deleteProduct } from "../../redux/actions";
 import { Card } from "flowbite-react/lib/esm/components";
+import { buttonclass } from '../../Style/Clases/Clases'
 import PrettyRating from "pretty-rating-react";
 import {
     faStar,
@@ -71,11 +72,18 @@ export default function CardProduct({ formCard = false, comidaProps, onProducEdi
                             null
                             :
                             <div>
-                                <Link className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" to={`/product/${_id}`}>
-                                    Agregar al carrito
-                                </Link>
-                                <button className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleEdit}>edit</button>
-                                <button className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleDelete}>delete</button>
+                                <button className={buttonclass}>
+                                    <Link to={`/product/${_id}`}>
+                                        <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
+                                        </path></svg>
+                                    </Link>
+                                </button>
+                                <button className={buttonclass} onClick={handleEdit}>
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                                </button>
+                                <button className={buttonclass} onClick={handleDelete}>
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                                </button>
                             </div>
                     }
                 </div>
