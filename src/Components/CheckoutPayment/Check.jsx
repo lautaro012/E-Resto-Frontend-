b4e8673ae7b7794333efc79dba9fd6e7b9590d84
Robles Lautaro <lautaro0121@gmail.com>
2022-10-11 00:14:02 -0300

    payment

--- src/Components/CheckoutPayment/Check.jsx
+++ src/Components/CheckoutPayment/Check.jsx
@@ -1,14 +1,17 @@
 
 import axios from 'axios'
 import StripeCheckout from 'react-stripe-checkout'
-import { deleteItemFromCart } from "../../redux/actions";
-import { useAppDispatch } from '../../config';
+import { createOrder, deleteItemFromCart } from "../../redux/actions";
+import { useAppDispatch, useAppSelector } from '../../config';
+import { useNavigate } from 'react-router-dom';
 
 export default function Check({ precio }) {
 
   let publishableKey = 'pk_test_51Lde2sJXnqrwcfODw8cWGGVzyavpCNgaUXMhWTAbkGIJ3txhY9PVGuUzy9QPzQ5riddbQZdRADa3QTHxqhrSeSZq00dWuMhBM2'
+  const items = useAppSelector((state) => state.cart)
+  const user = useAppSelector(state => state.user)
   const dispatch = useAppDispatch()
-
+  const navigate = useNavigate()
   const payNow = async token => {
 
     try {
@@ -21,7 +24,31 @@ export default function Check({ precio }) {
         }
       })
       if (response.status === 200) {
+
+        let cantidad = items.map(item => {
+          return {
+          name: item.name,
+          cant: item.cantidad
+          }
+        })
+        let products = items.map(item => {
+          return item.name
+        })
+
+        let payload = {
+          user_id : user._id,
+          date: new Date().toString(),
+          payment: 'Stripe',
+          subtotal: precio,
+          paid: true,
+          description: 'no se para que poner description',
+          products: products,
+          cantidad: cantidad
+        }
+
+        dispatch(createOrder(payload))
         dispatch(deleteItemFromCart("All"))
+        alert('COMPRA EXITOSA')
       }
     } catch (error) {
       console.log(error)
