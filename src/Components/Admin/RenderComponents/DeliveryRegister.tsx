c6c96f951d6c2681c3d3c7f7ffe067b54c236dd9
Sergio Romero <phyrofyre@gmail.com>
2022-10-16 18:04:28 -0300

    estilos en cart, footer, pedidos, suscribe, profile, admin

--- src/Components/Admin/RenderComponents/DeliveryRegister.tsx
+++ src/Components/Admin/RenderComponents/DeliveryRegister.tsx
@@ -15,11 +15,11 @@ const DeliveryRegister = () => {
         password: ''
     })
 
-    const handleChange = (e:any) => {
-          setSecondPass(e.target.value)
-      };
+    const handleChange = (e: any) => {
+        setSecondPass(e.target.value)
+    };
 
-    const handleInput = (e:any) => {
+    const handleInput = (e: any) => {
         setInput({
             ...input,
             [e.target.name]: e.target.value
@@ -38,24 +38,24 @@ const DeliveryRegister = () => {
 
     }
 
-  return (
-    <div className='divDeliveryRegister'>
-        <h3 className={headerRegisterDelivery}>Registrar nuevo repartidor</h3>
-        <form className='formRegisterDeliveryStyle' onSubmit={handleSubmit}>
-            <input onChange={e => handleInput(e)} required name='name' type="text" placeholder='Ingrese nombre aquí' className={inputRegisterDelivery} ></input> 
-            <input onChange={e => handleInput(e)} required name='lastName' type="text" placeholder='Ingrese apellido aquí' className={inputRegisterDelivery} ></input> 
-            <div className="relative mb-6" id='divMailInput'>
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
-            </div>
-            <input required type="email" name='mail' onChange={e => handleInput(e)} className={inputRegisterDelivery} placeholder="name@henrysfood.com"/>
-            </div>            
-            <input required onChange={e => handleInput(e)} name='password' type="password" placeholder='Ingrese la contraseña aquí' className={inputRegisterDelivery} ></input> 
-            <input required onChange={e => handleChange(e)} name='repeatPass' type="password" placeholder='Repita la contraseña' className={inputRegisterDelivery} ></input> 
-            <button type='submit' className={buttonclass}>Registrar repartidor</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className='divDeliveryRegister' data-aos="fade-left" data-aos-duration="500">
+            <h3 className={headerRegisterDelivery}>Registrar nuevo repartidor</h3>
+            <form className='formRegisterDeliveryStyle' onSubmit={handleSubmit}>
+                <input onChange={e => handleInput(e)} required name='name' type="text" placeholder='Ingrese nombre aquí' className={inputRegisterDelivery} ></input>
+                <input onChange={e => handleInput(e)} required name='lastName' type="text" placeholder='Ingrese apellido aquí' className={inputRegisterDelivery} ></input>
+                <div className="relative mb-6" id='divMailInput'>
+                    <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                    </div>
+                    <input required type="email" name='mail' onChange={e => handleInput(e)} className={inputRegisterDelivery} placeholder="name@henrysfood.com" />
+                </div>
+                <input required onChange={e => handleInput(e)} name='password' type="password" placeholder='Ingrese la contraseña aquí' className={inputRegisterDelivery} ></input>
+                <input required onChange={e => handleChange(e)} name='repeatPass' type="password" placeholder='Repita la contraseña' className={inputRegisterDelivery} ></input>
+                <button type='submit' className={buttonclass}>Registrar repartidor</button>
+            </form>
+        </div>
+    )
 }
 
 export default DeliveryRegister
--- src/Components/Admin/RenderComponents/UserList.tsx
+++ src/Components/Admin/RenderComponents/UserList.tsx
@@ -5,149 +5,154 @@ import { useAppDispatch, useAppSelector } from '../../../config'
 import { changeBanUser, changeNoBanUser, changeUserAsAdmin, getAllUsers } from '../../../redux/actions'
 import { buttonclass, listUsuariosRegistrados, mailUsuarioRegistrado, nameUsuarioRegistrado, titleUsuariosRegistrados } from '../../../Style/Clases/Clases'
 
-const UserList = (render : any) => {
+const UserList = (render: any) => {
     let dispatch = useAppDispatch()
     const users = useAppSelector((state) => state.allUsers)
     const [prueba, setPrueba] = useState('')
-    
+
     const getRandom = () => {
-        let num = Math.random()*Math.random()
+        let num = Math.random() * Math.random()
         return num.toString()
     }
 
-    const handleBan = (e : any) => {
+    const handleBan = (e: any) => {
         dispatch(changeBanUser(e.target.value))
         setPrueba(getRandom())
     }
-    
+
     const handleAdmin = (e: any) => {
         dispatch(changeUserAsAdmin(e.target.value))
         setPrueba(getRandom())
     }
 
-    const undoBanUser = (e:any) => {
+    const undoBanUser = (e: any) => {
         dispatch(changeNoBanUser(e.target.value))
         setPrueba(getRandom())
 
-    } 
+    }
 
-// console.log(users)
     useEffect(() => {
         dispatch(getAllUsers())
     }, [dispatch, prueba])
 
-    
-
-
-
-  return (
-    <div>
-    <div id='sizingDivAdmin'>
-        <div className="flex justify-between items-center mb-4">
-            <h5 className={titleUsuariosRegistrados}>Usuarios Registrados</h5>
-        </div>
-        {
-                render.render === 'registeredUsers' ?
-                users?.filter((e : any) => e.baneado === false)
-                .filter((e:any) => e.admin === false)
-                .map((user : any) => {
-                    return (
-                        
-                        <div className="flow-root" key={user._id}>
-                                <ul role="list" className={listUsuariosRegistrados}>
-                                    <li className="py-3 sm:py-4">
-                                        <div className="flex items-center space-x-4">
-                                            <div className="flex-shrink-0">
-                                                <img className="w-8 h-8 rounded-full" src={user.img} alt="User image"/>
+    return (
+        <div data-aos="fade-left" data-aos-duration="500">
+            <div id='sizingDivAdmin'>
+                {
+                    render.render === 'registeredUsers' ?
+                        <div data-aos="fade-left" data-aos-duration="500">
+                            <div className="flex justify-between items-center mb-4">
+                                <h5 className={titleUsuariosRegistrados}>Usuarios Registrados</h5>
+                            </div>
+                            {
+                                users?.filter((e: any) => e.baneado === false)
+                                    .filter((e: any) => e.admin === false)
+                                    .map((user: any) => {
+                                        return (
+                                            <div className="flow-root" key={user._id} data-aos="fade-left" data-aos-duration="500">
+                                                <ul role="list" className={listUsuariosRegistrados}>
+                                                    <li className="py-3 sm:py-4">
+                                                        <div className="flex items-center space-x-4">
+                                                            <div className="flex-shrink-0">
+                                                                <img className="w-8 h-8 rounded-full" src={user.img} alt="User image" />
+                                                            </div>
+                                                            <div className="flex-1 min-w-0">
+                                                                <p className={nameUsuarioRegistrado}>
+                                                                    {user.userName}
+                                                                </p>
+                                                                <p className={mailUsuarioRegistrado}>
+                                                                    {user.mail}
+                                                                </p>
+                                                            </div>
+                                                            <div>
+                                                                <button onClick={(e) => { handleBan(e) }} value={user._id} className={buttonclass}>Banear</button>
+                                                                <button className={buttonclass} value={user._id} onClick={(e) => handleAdmin(e)}>Hacer Admin</button>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
                                             </div>
-                                            <div className="flex-1 min-w-0">
-                                                <p className={nameUsuarioRegistrado}>
-                                                    {user.userName}
-                                                </p>
-                                                <p className={mailUsuarioRegistrado}>
-                                                    {user.mail}
-                                                </p>
-                                            </div>
-                                            <div>
-                                                <button onClick={(e) => {handleBan(e)}} value={user._id} className={buttonclass}>Banear</button>
-                                                <button className={buttonclass} value={user._id} onClick={(e) => handleAdmin(e)}>Hacer Admin</button>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
+                                        )
+                                    })
+                            }
                         </div>
-                    )
-                    })
-        : 
-        render.render === 'bannedUsers' ? 
-
-        users?.filter((e : any) => e.baneado === true)
-        .map((user : any) => {
-            return (
-                <div className="flow-root" key={user._id}>
-                        <ul role="list" className={listUsuariosRegistrados}>
-                            <li className="py-3 sm:py-4">
-                                <div className="flex items-center space-x-4">
-                                    <div className="flex-shrink-0">
-                                        <img className="w-8 h-8 rounded-full" src={user.img} alt="User image"/>
-                                    </div>
-                                    <div className="flex-1 min-w-0">
-                                        <p className={nameUsuarioRegistrado}>
-                                            {user.userName}
-                                        </p>
-                                        <p className={mailUsuarioRegistrado}>
-                                            {user.mail}
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <button onClick={(e) => {undoBanUser(e)}} value={user._id} className={buttonclass}>Deshacer Baneo</button>
-                                    </div>
+                        :
+                        render.render === 'bannedUsers' ?
+                            <div data-aos="fade-left" data-aos-duration="500">
+                                <div className="flex justify-between items-center mb-4">
+                                    <h5 className={titleUsuariosRegistrados}>Usuarios baneados</h5>
                                 </div>
-                            </li>
-                        </ul>
-                </div>
+                                {
+                                    users?.filter((e: any) => e.baneado === true)
+                                        .map((user: any) => {
+                                            return (
+                                                <div className="flow-root" key={user._id} data-aos="fade-left" data-aos-duration="500">
+                                                    <ul role="list" className={listUsuariosRegistrados}>
+                                                        <li className="py-3 sm:py-4">
+                                                            <div className="flex items-center space-x-4">
+                                                                <div className="flex-shrink-0">
+                                                                    <img className="w-8 h-8 rounded-full" src={user.img} alt="User image" />
+                                                                </div>
+                                                                <div className="flex-1 min-w-0">
+                                                                    <p className={nameUsuarioRegistrado}>
+                                                                        {user.userName}
+                                                                    </p>
+                                                                    <p className={mailUsuarioRegistrado}>
+                                                                        {user.mail}
+                                                                    </p>
+                                                                </div>
+                                                                <div>
+                                                                    <button onClick={(e) => { undoBanUser(e) }} value={user._id} className={buttonclass}>Deshacer Baneo</button>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </div>
 
-                ) 
-            })
-        
-        :
-
-        users?.filter((e : any) => e.admin === true && e.baneado === false)
-        .map((user : any) => {
-            return (
-                <div className="flow-root" key={user._id}>
-                        <ul role="list" className={listUsuariosRegistrados}>
-                            <li className="py-3 sm:py-4">
-                                <div className="flex items-center space-x-4">
-                                    <div className="flex-shrink-0">
-                                        <img className="w-8 h-8 rounded-full" src={user.img} alt="User image"/>
-                                    </div>
-                                    <div className="flex-1 min-w-0">
-                                        <p className={nameUsuarioRegistrado}>
-                                            {user.userName}
-                                        </p>
-                                        <p className={mailUsuarioRegistrado}>
-                                            {user.mail}
-                                        </p>
-                                    </div>
-                                    <div>
-                                    <button onClick={(e) => {handleBan(e)}} value={user._id} className={buttonclass}>Banear Usuario</button>
-                                    </div>
+                                            )
+                                        })
+                                }
+                            </div>
+                            :
+                            <div data-aos="fade-left" data-aos-duration="500">
+                                <div className="flex justify-between items-center mb-4">
+                                    <h5 className={titleUsuariosRegistrados}>Administradores</h5>
                                 </div>
-                            </li>
-                        </ul>
-                </div>
+                                {
+                                    users?.filter((e: any) => e.admin === true && e.baneado === false)
+                                        .map((user: any) => {
+                                            return (
+                                                <div className="flow-root" key={user._id} data-aos="fade-left" data-aos-duration="500">
+                                                    <ul role="list" className={listUsuariosRegistrados}>
+                                                        <li className="py-3 sm:py-4">
+                                                            <div className="flex items-center space-x-4">
+                                                                <div className="flex-shrink-0">
+                                                                    <img className="w-8 h-8 rounded-full" src={user.img} alt="User image" />
+                                                                </div>
+                                                                <div className="flex-1 min-w-0">
+                                                                    <p className={nameUsuarioRegistrado}>
+                                                                        {user.userName}
+                                                                    </p>
+                                                                    <p className={mailUsuarioRegistrado}>
+                                                                        {user.mail}
+                                                                    </p>
+                                                                </div>
+                                                                <div>
+                                                                    <button onClick={(e) => { handleBan(e) }} value={user._id} className={buttonclass}>Banear Usuario</button>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </div>
 
-                ) 
-            
-            })
-            
-        
-        
-        }
-    </div>
-        
-    </div>
+                                            )
+                                        })
+                                }
+                            </div>
+                }
+            </div>
+
+        </div>
     )
 }
 
